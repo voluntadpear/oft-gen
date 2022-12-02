f5df55fae764a50dac69bc1d28c547946987a0c2
Guillermo Peralta Scura <gperaltascura@gmail.com>
2022-12-02 11:56:52 -0300

    Migrate to npm specifiers

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { Client } from "https://esm.sh/@notionhq/client@2.2.0";
+export { Client } from "npm:@notionhq/client@2.2.0";
 export type {
   PageObjectResponse,
   QueryDatabaseResponse,
   TextRichTextItemResponse,
-} from "https://esm.sh/v94/@notionhq/client@2.2.0/build/src/api-endpoints.d.ts";
+} from "npm:@notionhq/client@2.2.0/build/src/api-endpoints.d.ts";
 export { writeText as writeTextToClipboard } from "https://deno.land/x/copy_paste@v1.1.3/mod.ts";
